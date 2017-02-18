be6fa11c04415f28070e07b5ddef393b3a7b218e
Yaokun Pan <myron.yk.pan@gmail.com>
2017-02-18 23:41:46 +1100

    Initial Commit

---
+++ src/app/+game/game.component.ts
@@ -0,0 +1,19 @@
+/**
+ * game.component
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-game',
+    templateUrl: 'game.component.html'
+})
+export class GameComponent implements OnInit {
+    constructor() {
+
+    }
+
+    public ngOnInit() {
+
+    }
+}
