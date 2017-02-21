9c796c40a44c4fe54ae77322a6744492f42fc86c
Yaokun Pan <myron.yk.pan@gmail.com>
2017-02-22 00:07:46 +1100

    Added app-inform

--- src/app/+game/game-quiz/home/home.component.ts
+++ src/app/+game/game-quiz/home/home.component.ts
@@ -11,6 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class GameHomeComponent implements OnInit {
+
+    public socialBtns: Array<{path: string, href: string, caption: string}> = [
+        {
+            path: '/assets/img/game/2048/github.svg',
+            href: 'https://github.com/DanielYKPan',
+            caption: 'Fork me on Github'
+        },
+        {
+            path: '/assets/img/game/2048/twitter.svg',
+            href: 'https://twitter.com/DanielYKPan',
+            caption: 'Check my Twitter'
+        },
+        {
+            path: '/assets/img/game/2048/paper-plane.svg',
+            href: 'mailto:myron.yk.pan@gmail.com',
+            caption: 'Send an Email'
+        },
+    ];
+
     constructor() {
     }
 
