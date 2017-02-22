9bbdfeb6ea2a1c8b2096f483848d2a0c10b26900
Yaokun Pan <myron.yk.pan@gmail.com>
2017-02-22 12:21:57 +1100

    Change game quiz social btn image path

--- src/app/+game/game-quiz/home/home.component.ts
+++ src/app/+game/game-quiz/home/home.component.ts
@@ -14,17 +14,17 @@ export class GameHomeComponent implements OnInit {
 
     public socialBtns: Array<{path: string, href: string, caption: string}> = [
         {
-            path: '/assets/img/game/2048/github.svg',
+            path: '/assets/img/game/social/github.svg',
             href: 'https://github.com/DanielYKPan',
             caption: 'Fork me on Github'
         },
         {
-            path: '/assets/img/game/2048/twitter.svg',
+            path: '/assets/img/game/social/twitter.svg',
             href: 'https://twitter.com/DanielYKPan',
             caption: 'Check my Twitter'
         },
         {
-            path: '/assets/img/game/2048/paper-plane.svg',
+            path: '/assets/img/game/social/paper-plane.svg',
             href: 'mailto:myron.yk.pan@gmail.com',
             caption: 'Send an Email'
         },
